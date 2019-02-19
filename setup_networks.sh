f60c5358f67a36ed265f332c4dd6bd6f31e89f91
Benjamin Binnard <benjaminbinnard@gmail.com>
2019-02-19 19:37:31 +0100

    More stuff in the startup file

--- setup_networks.sh
+++ setup_networks.sh
@@ -6,10 +6,15 @@ interfaces=$(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n' 
 
 while read -r link; do
     bridgename="dckr-$link"
+    echo "Removing old network $bridgename"
     docker network rm $bridgename
+    echo "Adding new network $bridgename"
     bridge=$(docker network create --attachable --opt "com.docker.network.bridge.name=$bridgename" --opt "com.docker.network.bridge.enable_ip_masquerade=false" $bridgename)
-    if_ip=ip -br -4 address show dev $link scope global | awk '{split($3,a,"/"); print a[1]}'
+    echo "Fetching interface $link's IPv4 address"
+    if_ip=$(ip -br -4 address show dev $link scope global | awk '{split($3,a,"/"); print a[1]}')
+    echo "Fetching $bridgename's ip"
     bridge_ip=$(docker network inspect $bridge | jq '.[0].IPAM.Config[0].Subnet')
+    echo "Modifying iptables rules for $bridgename"
     iptables -t nat -A POSTROUTING -s $bridge_ip ! -o $bridgename -j SNAT --to-source $if_ip
     echo "Created bridge $bridgename for interface $link with bridge ip $bridge_ip and interface ip $if_ip"
 done <<< "$interfaces"

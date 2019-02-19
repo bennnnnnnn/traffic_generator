36b0c4630975818d2522ef323bc1b9636fb3bdfb
Benjamin Binnard <benjaminbinnard@gmail.com>
2019-02-19 20:12:14 +0100

    More stuff in the startup file

--- setup_networks.sh
+++ setup_networks.sh
@@ -8,6 +8,9 @@ fi
 #apt-get update
 apt-get install -y jq
 
+#bugfix for the Realtek r8152 chipset
+ethtool -s eth1 speed 100 duplex full autoneg off
+
 ./wifi_connect.sh
 
 interfaces=$(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n' | grep -v "eth0")
@@ -21,10 +24,11 @@ while read -r link; do
     echo "Fetching interface $link's IPv4 address"
     if_ip=$(ip -br -4 address show dev $link scope global | awk '{split($3,a,"/"); print a[1]}')
     echo "Fetching $bridgename's ip"
-    bridge_ip=$(docker network inspect $bridge | jq '.[0].IPAM.Config[0].Subnet')
+    bridge_nw=$(docker network inspect $bridge | jq '.[0].IPAM.Config[0].Subnet')
+    bridge_nw="${$bridgeip/\"/}"
     echo "Modifying iptables rules for $bridgename"
-    iptables -t nat -A POSTROUTING -s $bridge_ip ! -o $bridgename -j SNAT --to-source $if_ip
-    echo "Created bridge $bridgename for interface $link with bridge ip $bridge_ip and interface ip $if_ip"
+    iptables -t nat -A POSTROUTING -s $bridge_nw ! -o $bridgename -j SNAT --to-source $if_ip
+    echo "Created bridge $bridgename for interface $link with bridge ip $bridge_nw and interface ip $if_ip"
 done <<< "$interfaces"
 
 

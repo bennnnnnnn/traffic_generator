c1276d93eeb17377d2f6ddc641adcd04d579b27f
Benjamin Binnard <benjaminbinnard@gmail.com>
2019-02-19 19:41:20 +0100

    More stuff in the startup file

--- setup_networks.sh
+++ setup_networks.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+apt-get update
+apt-get install -y jq
+
 ./wifi_connect.sh
 
 interfaces=$(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n' | grep -v "eth0")
